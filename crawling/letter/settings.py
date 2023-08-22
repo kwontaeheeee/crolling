2e924bc862e7f4eeb07b60188bcd15a8f29010a4
dasiy <dasiy1203@naver.com>
2023-08-22 17:12:06 +0900

    first

--- crawling/letter/settings.py
+++ crawling/letter/settings.py
@@ -3,6 +3,6 @@ from pathlib import path
 
 SATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'myprofile', 'static')]
+    os.path.join(BASE_DIR, 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
