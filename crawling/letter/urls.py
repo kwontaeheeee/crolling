0738756fc903d7801fbd837f020231ab0ae220d2
dasiy <dasiy1203@naver.com>
2023-08-23 18:15:57 +0900

    first

--- crawling/letter/urls.py
+++ crawling/letter/urls.py
@@ -1,13 +1,10 @@
-
 from django.urls import path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
-  path('', views.letter_log, name='letter_log')
-]
+  path('', views.letter_log, name='letter_main2'),
+  path('write/', views.letter_write, name='letter_write'),
+  ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
